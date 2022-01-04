6aa9b046747f7184521b737d2da5397cc06d3c93
TrigoZin <guilherme_zin@msn.com>
2022-01-04 10:41:39 -0300

    Corrigindo bug

--- Teste-1-isSubsequent.js
+++ Teste-1-isSubsequent.js
@@ -3,24 +3,24 @@ const args2 = [1, 6, -1, 10];
 console.log(isSubsequent(args, args2));
 
 function isSubsequent(args, args2) {
-    const newArray = [];
-    
-    args2.forEach(elementArgs2 => {
-        args.forEach(elementArgs1 => {
-            if(elementArgs1 === elementArgs2)
-                newArray.push(elementArgs1);
-                return              
-        })
-    });
+  const newArray = [];
 
-    return compareTwoArrays(newArray, args2);
+  args.forEach(elementArgs1 => {
+    args2.forEach(elementArgs2 => {
+      if (elementArgs1 === elementArgs2)
+        newArray.push(elementArgs1);
+      return
+    })
+  });
+
+  return compareTwoArrays(newArray, args2);
 }
 
-function compareTwoArrays(args1, args2){
-    var i = args1.length;
-    while (i--) {
-      if (args1[i] !== args2[i]) return false;
-    }
+function compareTwoArrays(args1, args2) {
+  var i = args1.length;
+  while (i--) {
+    if (args1[i] !== args2[i]) return false;
+  }
 
   return true
 }
